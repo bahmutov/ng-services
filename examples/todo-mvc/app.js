b1b49b9c15b23f3c63141de79a18174ad061c7b4
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2015-10-23 12:44:24 -0400

    adding todos

--- examples/todo-mvc/app.js
+++ examples/todo-mvc/app.js
@@ -2,23 +2,37 @@
   'use strict';
 
   var h = virtualDom.h;
+  // will be initialized later
+  var renderApp;
 
-  function renderHeader() {
+  function renderHeader(Todos) {
+    function isEnter(e) {
+      return e.keyCode === 13;
+    }
+    function onKey(e) {
+      if (isEnter(e)) {
+        Todos.add(e.target.value);
+        e.target.value = '';
+        renderApp();
+      }
+    }
+
     return h('header', {className: 'header'}, [
-      h('h1',{}, ['todos']),
+      h('h1',{}, 'todos'),
       h('input', {
         className: 'new-todo', 
         placeholder: 'What needs to be done?',
-        autofocus: true
+        autofocus: true,
+        onkeyup: onKey
       }, [])
     ]);
   }
 
-  function renderToggleAndTodos(todos) {
-    todos = todos || [];
-    
+  function renderToggleAndTodos(Todos) {
+    var items = Todos ? Todos.all : [];
+
     function toDom(todo) {
-      return h('li', [
+      return h('li', {key: todo.id}, [
         h('div', {className: 'view'}, [
           h('input', {className: 'toggle', type: 'checkbox'}),
           h('label', todo.what)
@@ -29,7 +43,7 @@
     return h('section', {className: 'main'}, [
       h('input', {className: 'toggle-all', type: 'checkbox'}, []),
       h('label', {htmlFor: 'toggle-all'}, ['Mark all as complete']),
-      h('ul', {className: 'todo-list'}, todos.map(toDom))
+      h('ul', {className: 'todo-list'}, items.map(toDom))
     ]);
   }
 
@@ -52,9 +66,8 @@
   }
 
   function render(todos) {
-    la(check.maybe.array(todos), 'missing list of todos to render', todos);
     return h('section', {className: 'todoapp'}, [
-      renderHeader(),
+      renderHeader(todos),
       renderToggleAndTodos(todos),
       renderFooter()
     ]);
@@ -67,8 +80,8 @@
     rootNode = virtualDom.create(view);
     document.querySelector('#app').appendChild(rootNode);
 
-    function renderApp(todos) {
-      var newView = render(todos);
+    function renderApp(Todos) {
+      var newView = render(Todos);
       var patches = virtualDom.diff(view, newView);
       rootNode = virtualDom.patch(rootNode, patches);
       view = newView;
@@ -77,14 +90,13 @@
     return renderApp;
   }
 
-  var renderApp = initRender();
   ngServices({
     src: 'ng-todos.js',
     module: 'Todos',
   }).then(function (Todos) {
-    renderApp(Todos.all);
-    Todos.add('learn Italian');
-    renderApp(Todos.all);
+    renderApp = initRender().bind(null, Todos);
+    // Todos.add('learn Italian');
+    renderApp();
   });
 
 }());
